b92d35e37b91789b19ad8159fdf9b19e92acbb02
Claire Matranga <cmatranga7@Claires-MacBook-Pro.local>
2020-09-06 15:06:50 -0400

    added set of test parameters and rewrote test_find_y to reflect changes to funtion to be tested

--- test_y_coord.py
+++ test_y_coord.py
@@ -1,7 +1,13 @@
+import pytest
 import find_y from y_coord
 
-def test_find_y(){
-    answer = find_y((0,0), (5, 5), 3);
-    expected = 3
-    assert answer == expected
+@pytest.mark.parametrize("(x1, y2), (x2, y2), x3, expected_m, expected_y3", [
+    ((0, 0), (2, 2), 1, 1, 1),
+    ((1, 6), (5, 8), 3, 0.5, 7),
+    ((0, 10), (10, 12), 30, 1/5, 16),
+    ])
+
+def test_find_y((x1, y2), (x2, y2), x3, expected_y3){
+    answer = find_y(((x1, y2), (x2, y2), x3);
+    assert answer == expected_y3
 }
