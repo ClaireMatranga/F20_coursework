c9a8cb28a80c24b7156615f86b6dbc72301a5de8
Claire Matranga <cmatranga7@Claires-MacBook-Pro.local>
2020-09-06 15:09:31 -0400

    developed test fn for find_m fn

--- test_y_coord.py
+++ test_y_coord.py
@@ -1,5 +1,5 @@
 import pytest
-import find_y from y_coord
+import find_y, find_slope, find_y_3 from y_coord
 
 @pytest.mark.parametrize("(x1, y2), (x2, y2), x3, expected_m, expected_y3", [
     ((0, 0), (2, 2), 1, 1, 1),
@@ -7,7 +7,10 @@ import find_y from y_coord
     ((0, 10), (10, 12), 30, 1/5, 16),
     ])
 
-def test_find_y((x1, y2), (x2, y2), x3, expected_y3){
+def test_find_y((x1, y2), (x2, y2), x3, expected_y3):
     answer = find_y(((x1, y2), (x2, y2), x3);
     assert answer == expected_y3
-}
+
+def test_find_m((x1, y1), (x2, y2), expected_m):
+    answer = find_slope((x1, y1), (x2, y2))
+    assert answer == expected_m
